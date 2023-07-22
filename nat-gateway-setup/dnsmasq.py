f3fb9105ac8c9a744ee38d0435489105e84a55bc
Brandon Geraci <brandon.geraci@gmail.com>
2023-07-22 12:02:15 -0400

    Adding checks and restart to dnsmasq

--- nat-gateway-setup/dnsmasq.py
+++ nat-gateway-setup/dnsmasq.py
@@ -5,6 +5,7 @@ import glob
 from .utils import CustomException
 import tarfile
 import time
+import re
 
 def backup_dnsmasq_configuration(backup_folder="/var/cache/nat-linux-gateway"):
     """
@@ -44,26 +45,64 @@ def restore_dnsmasq_configuration(backup_folder="/var/cache/nat-linux-gateway", 
         except Exception as e:
             raise CustomException(f"Restoring dnsmasq configuration failed with error: {str(e)}") from None
 
-def configure_dnsmasq(dns_interface, dns_range):
+def check_dnsmasq_configs(dns_interface):
     """
-    Configure DNSMasq.
+    Check if there are conflicting configurations in dnsmasq settings.
 
-    :param dns_interface: The network interface for DNSMasq to bind.
-    :param dns_range: The IP range for DNSMasq to assign IPs from.
+    :param dns_interface: The network interface that dnsmasq should bind to.
+    :raise: CustomException if a conflicting configuration is found
+    """
+    # Check if there are conflicting configurations in /etc/dnsmasq.conf
+    with open('/etc/dnsmasq.conf', 'r') as f:
+        contents = f.read()
+        if re.search(rf'interface={dns_interface}', contents):
+            raise CustomException(f"Conflict found in /etc/dnsmasq.conf for interface {dns_interface}")
+
+    # Check if there are conflicting configurations in /etc/dnsmasq.d/
+    for filename in glob.glob('/etc/dnsmasq.d/*.conf'):
+        with open(filename, 'r') as f:
+            contents = f.read()
+            if re.search(rf'interface={dns_interface}', contents):
+                raise CustomException(f"Conflict found in {filename} for interface {dns_interface}")
+
+
+def restart_dnsmasq():
+    """
+    Restart the dnsmasq service.
+
+    :raise: CustomException if the service fails to restart
+    """
+    try:
+        subprocess.check_call(['systemctl', 'restart', 'dnsmasq'])
+    except subprocess.CalledProcessError as e:
+        raise CustomException(f"Failed to restart dnsmasq service: {str(e)}") from None
+
+
+def configure_dnsmasq(dns_interface, ip_range, dns_server, lease_time):
+    """
+    Configure dnsmasq with the given parameters.
+
+    :param dns_interface: The network interface that dnsmasq should bind to.
+    :param ip_range: The IP range for DHCP.
+    :param dns_server: The upstream DNS server.
+    :param lease_time: The DHCP lease time.
     :raise: CustomException if the configuration fails
     """
     try:
-        backup_dnsmasq_configuration()
-        
+        # First, check for conflicts
+        check_dnsmasq_configs(dns_interface)
+
+        # Write our new configuration
         with open(f'/etc/dnsmasq.d/nat_{dns_interface}.conf', 'w') as f:
-            f.write(f"""
-                bind-interfaces
-                interface={dns_interface}
-                except-interface=lo
-                dhcp-range={dns_range}
-            """)
+            f.write(f"interface={dns_interface}\n")
+            f.write(f"dhcp-range={ip_range},{lease_time}\n")
+            f.write(f"dhcp-option=6,{dns_server}\n")
+            f.write("no-resolv\n")
+            f.write("no-poll\n")
+            f.write("bind-interfaces\n")
 
-        subprocess.check_call(['systemctl', 'restart', 'dnsmasq'])
+        # Restart the dnsmasq service to apply changes
+        restart_dnsmasq()
 
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         raise CustomException(f"DNSMasq configuration failed with error: {str(e)}") from None
