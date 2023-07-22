aaa048a8d314e8ae0715f3452b90972fda47fabb
Brandon Geraci <brandon.geraci@gmail.com>
2023-07-22 14:40:09 -0400

    Adding ip range

--- nat_gateway_setup/nat_gateway_setup.py
+++ nat_gateway_setup/nat_gateway_setup.py
@@ -15,11 +15,18 @@ class IPValidator(Validator):
         except ValueError:
             raise ValidationError(message="Please enter a valid IP address", cursor_position=len(document.text))
 
+class IntValidator(Validator):
+    def validate(self, document):
+        if not document.text.isdigit():
+            raise ValidationError(message="Please enter a valid integer", cursor_position=len(document.text))
+
 def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--ip', help='the IP address')
     parser.add_argument('--netmask', help='the netmask')
+    parser.add_argument('--dhcp-start-ip', help='the start IP of the DHCP range')
+    parser.add_argument('--dhcp-ip-count', type=int, help='the number of IPs in the DHCP range')
     parser.add_argument('--wlan', help='the incoming network interface (WLAN)')
     parser.add_argument('--lan', help='the local network interface (LAN)')
     args = parser.parse_args()
@@ -33,6 +40,8 @@ def main():
     # If arguments are not provided via command line, ask for user input
     ip = args.ip if args.ip else prompt("Please enter the IP address: ", validator=IPValidator())
     netmask = args.netmask if args.netmask else prompt("Please enter the netmask: ")  # add validation if desired
+    dhcp_start_ip = args.dhcp_start_ip if args.dhcp_start_ip else prompt("Please enter the start IP of the DHCP range: ", validator=IPValidator())
+    dhcp_ip_count = args.dhcp_ip_count if args.dhcp_ip_count else prompt("Please enter the number of IPs in the DHCP range: ", validator=IntValidator())
     wlan_interface = args.wlan if args.wlan else prompt("Please enter the incoming network interface (WLAN): ")
     lan_interface = args.lan if args.lan else prompt("Please enter the local network interface (LAN): ")
 
@@ -41,7 +50,8 @@ def main():
 
     # Setup dnsmasq
     check_dnsmasq_configs(lan_interface)
-    configure_dnsmasq(lan_interface, ip, netmask)
+    ip_range = f"{dhcp_start_ip},{dhcp_start_ip + dhcp_ip_count - 1}"
+    configure_dnsmasq(lan_interface, ip_range, netmask)
     restart_dnsmasq()
 
     # Setup firewall
