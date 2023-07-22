6fbf9f7538f8db7528f03036a320ef481de47633
Brandon Geraci <brandon.geraci@gmail.com>
2023-07-22 11:53:17 -0400

    Adding backup and restore dnsmasq

--- nat-gateway-setup/dnsmasq.py
+++ nat-gateway-setup/dnsmasq.py
@@ -1,33 +1,69 @@
 import os
+import shutil
 import subprocess
+import glob
 from .utils import CustomException
+import tarfile
+import time
 
-class DNSMasqManager:
+def backup_dnsmasq_configuration(backup_folder="/var/cache/nat-linux-gateway"):
+    """
+    Backup existing dnsmasq configuration.
 
-    def install_dnsmasq(self):
-        """
-        Install DNSMasq using the appropriate package manager.
+    :param backup_folder: The directory to store the backup.
+    :raise: CustomException if the backup fails
+    """
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    backup_file = os.path.join(backup_folder, f'dnsmasq_backup_{timestamp}.tar.gz')
 
-        :raise: CustomException if the installation fails
-        """
+    with tarfile.open(backup_file, 'w:gz') as tar:
         try:
-            if os.path.isfile('/usr/bin/yum'):
-                subprocess.check_call(['yum', 'install', '-y', 'dnsmasq'])
-            elif os.path.isfile('/usr/bin/apt-get'):
-                subprocess.check_call(['apt-get', 'install', '-y', 'dnsmasq'])
-            else:
-                raise CustomException("Unsupported package manager. Please manually install dnsmasq.")
-        except subprocess.CalledProcessError as e:
-            raise CustomException(f"DNSMasq installation failed with error: {str(e)}") from None
+            tar.add('/etc/dnsmasq.conf')
+            tar.add('/etc/dnsmasq.d/')
+        except Exception as e:
+            raise CustomException(f"Backup of existing dnsmasq configuration failed with error: {str(e)}") from None
 
-    def configure_dnsmasq(self):
-        """
-        Configure DNSMasq.
 
-        :raise: CustomException if the configuration fails
-        """
+def restore_dnsmasq_configuration(backup_folder="/var/cache/nat-linux-gateway", backup_file=None):
+    """
+    Restore dnsmasq configuration from a backup.
+
+    :param backup_folder: The directory where the backup is stored.
+    :param backup_file: The tar.gz file to restore from. If None, uses the latest backup.
+    :raise: CustomException if the restore fails
+    """
+    if backup_file is None:
+        backup_files = sorted(os.listdir(backup_folder))
+        if not backup_files:
+            raise CustomException("No backup files found.")
+        backup_file = os.path.join(backup_folder, backup_files[-1])
+
+    with tarfile.open(backup_file, 'r:gz') as tar:
         try:
-            # Add your dnsmasq configuration commands here
-            # Example: subprocess.check_call(['systemctl', 'restart', 'dnsmasq'])
-        except subprocess.CalledProcessError as e:
-            raise CustomException(f"DNSMasq configuration failed with error: {str(e)}") from None
+            tar.extractall(path='/etc/')
+        except Exception as e:
+            raise CustomException(f"Restoring dnsmasq configuration failed with error: {str(e)}") from None
+
+def configure_dnsmasq(dns_interface, dns_range):
+    """
+    Configure DNSMasq.
+
+    :param dns_interface: The network interface for DNSMasq to bind.
+    :param dns_range: The IP range for DNSMasq to assign IPs from.
+    :raise: CustomException if the configuration fails
+    """
+    try:
+        backup_dnsmasq_configuration()
+        
+        with open(f'/etc/dnsmasq.d/nat_{dns_interface}.conf', 'w') as f:
+            f.write(f"""
+                bind-interfaces
+                interface={dns_interface}
+                except-interface=lo
+                dhcp-range={dns_range}
+            """)
+
+        subprocess.check_call(['systemctl', 'restart', 'dnsmasq'])
+
+    except subprocess.CalledProcessError as e:
+        raise CustomException(f"DNSMasq configuration failed with error: {str(e)}") from None
